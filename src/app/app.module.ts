0c488ffe9ca2b4f1df57374ff9b5d0173ca10958
Nishant <nishant817@outlook.com>
2019-06-14 07:12:29 -0500

    Updated to Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopnavComponent } from '../components/topnav/topnav.component';
+import { AboutComponent } from '../components/about/about.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { EducationComponent } from '../components/education/education.component';
+import { ProfComponent } from '../components/prof/prof.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { SkillsComponent } from '../components/skills/skills.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopnavComponent,
+    AboutComponent,
+    ContactComponent,
+    EducationComponent,
+    ProfComponent,
+    ProjectsComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule.forRoot(),
+    TabsModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/components/topnav/topnav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'topnav',
+  templateUrl: './topnav.component.html',
+  styleUrls: ['./topnav.component.css']
+})
+export class TopnavComponent {
+  
+}
